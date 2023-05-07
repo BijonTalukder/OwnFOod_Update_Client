8ef6e5f5de1dd0407b749578ac4db2d9ab6d7cda
BijonTalukder <bijontalukder1247@gmail.com>
2023-05-07 11:19:10 +0600

    b

--- src/Components/Modal/ModalCheckOut.js
+++ src/Components/Modal/ModalCheckOut.js
@@ -6,25 +6,23 @@ const ModalCheckOut = (props) => {
     <>
       <Modal
         {...props}
-        size='lg'
+        size='md'
         aria-labelledby='contained-modal-title-vcenter'
         centered
       >
         <Modal.Header closeButton>
-          <Modal.Title id='contained-modal-title-vcenter'>
-            Modal heading
-          </Modal.Title>
+          <Modal.Title id='contained-modal-title-vcenter'></Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <div>
-            <div>
+            <div className='d-flex flex-column gap-3'>
               <div className='d-flex justify-content-around align-content-center'>
                 <h6
                   className='mx-1 pe-2 font-bold
                 '
                 >
                   {" "}
-                  Country
+                  Country &nbsp;
                 </h6>
                 <select
                   class='form-select form-select-sm'
@@ -36,7 +34,43 @@ const ModalCheckOut = (props) => {
                   <option value='3'>Three</option>
                 </select>
               </div>
-              <div></div>
+              <div className='d-flex justify-justify-content-between align-content-center'>
+                <h6
+                  className='mx-1 pe-2 font-bold
+                '
+                >
+                  {" "}
+                  City &nbsp;
+                </h6>
+                &nbsp; &nbsp; &nbsp; &nbsp;
+                <select
+                  class='form-select form-select-sm'
+                  aria-label='Default select example'
+                >
+                  <option selected>Open this select menu</option>
+                  <option value='1'>One</option>
+                  <option value='2'>Two</option>
+                  <option value='3'>Three</option>
+                </select>
+              </div>
+              <div className='d-flex justify-content-around align-content-center'>
+                <h6
+                  className='mx-1 pe-2 font-bold
+                '
+                >
+                  {" "}
+                  Region &nbsp;
+                </h6>
+                <select
+                  class='form-select form-select-sm'
+                  aria-label='Default select example'
+                >
+                  <option selected>Open this select menu</option>
+                  <option value='1'>One</option>
+                  <option value='2'>Two</option>
+                  <option value='3'>Three</option>
+                </select>
+              </div>
             </div>
           </div>
         </Modal.Body>
